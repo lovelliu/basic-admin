fa004a2702c14170a5ecf289c61efc35f19b68ae
lhj <1871731528@qq.com>
2021-11-26 14:48:49 +0800

    feat: add BasicForm component

--- src/components/Form/src/BasicForm.vue
+++ src/components/Form/src/BasicForm.vue
@@ -3,11 +3,11 @@
 </script>
 
 <script lang="ts" setup>
-  import { Ref, useAttrs } from 'vue';
+  import type { Ref } from 'vue';
   import type { AdvanceState } from './types/hooks';
   import type { FormSchema, FormActionType, FormProps } from './types/form';
 
-  import { reactive, ref, computed, unref } from 'vue';
+  import { reactive, ref, computed, unref, watch, useAttrs, nextTick, onMounted } from 'vue';
   import { Form, Row } from 'ant-design-vue';
   import { useDesign } from '/@/hooks/web/useDesign';
 
@@ -16,10 +16,15 @@
   import useAdvanced from './hooks/useAdvanced';
   import { useFormValues } from './hooks/useFormValues';
   import { useAutoFocus } from './hooks/useAutoFocus';
+  import { deepMerge } from '/@/utils';
+  import { useFormEvents } from './hooks/useFormEvents';
+  import { useModalContext } from '/@/components/Modal';
+  import { createFormContext } from './hooks/useFormContext';
 
   const props = defineProps(basicProps);
   const emit = defineEmits(['advanced-change', 'reset', 'submit', 'register']);
   const formModel = reactive<Recordable>({});
+  const modalFn = useModalContext();
 
   const advanceState = reactive<AdvanceState>({
     isAdvanced: true,
@@ -105,11 +110,154 @@
     isInitedDefault: isInitedDefaultRef,
     formElRef: formElRef as Ref<FormActionType>,
   });
+
+  const {
+    handleSubmit,
+    setFieldsValue,
+    clearValidate,
+    validate,
+    validateFields,
+    getFieldsValue,
+    updateSchema,
+    resetSchema,
+    appendSchemaByField,
+    removeSchemaByField,
+    resetFields,
+    scrollToField,
+  } = useFormEvents({
+    emit,
+    getProps,
+    formModel,
+    getSchema,
+    defaultValueRef,
+    formElRef: formElRef as Ref<FormActionType>,
+    schemaRef: schemaRef as Ref<FormSchema[]>,
+    handleFormValues,
+  });
+
+  createFormContext({
+    resetAction: resetFields,
+    submitAction: handleSubmit,
+  });
+
+  watch(
+    () => unref(getProps).model,
+    () => {
+      const { model } = unref(getProps);
+      if (!model) return;
+      setFieldsValue(model);
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  watch(
+    () => unref(getProps).schemas,
+    (schemas) => {
+      resetSchema(schemas ?? []);
+    },
+  );
+
+  watch(
+    () => getSchema.value,
+    (schema) => {
+      nextTick(() => {
+        modalFn?.redoModalHeight?.();
+      });
+      if (unref(isInitedDefaultRef)) return;
+      if (schema?.length) {
+        initDefault();
+        isInitedDefaultRef.value = true;
+      }
+    },
+  );
+
+  async function setProps(formProps: Partial<FormProps>): Promise<void> {
+    propsRef.value = deepMerge(unref(propsRef) || {}, formProps);
+  }
+
+  function setFormModel(key: string, value: any) {
+    formModel[key] = value;
+    const { validateTrigger } = unref(getBindValue);
+    if (!validateTrigger || validateTrigger === 'change') {
+      validateFields([key]).catch((_) => {});
+    }
+  }
+
+  function handleEnterPress(e: KeyboardEvent) {
+    const { autoSubmitOnEnter } = unref(getProps);
+    if (!autoSubmitOnEnter) return;
+    if (e.key === 'Enter' && e.target && e.target instanceof HTMLElement) {
+      const target: HTMLElement = e.target as HTMLElement;
+      if (target && target.tagName && target.tagName.toUpperCase() == 'INPUT') {
+        handleSubmit();
+      }
+    }
+  }
+
+  const formActionType: Partial<FormActionType> = {
+    getFieldsValue,
+    setFieldsValue,
+    resetFields,
+    updateSchema,
+    resetSchema,
+    setProps,
+    removeSchemaByField,
+    appendSchemaByField,
+    clearValidate,
+    validateFields,
+    validate,
+    submit: handleSubmit,
+    scrollToField: scrollToField,
+  };
+
+  onMounted(() => {
+    initDefault();
+    emit('register', formActionType);
+  });
+
+  const getFormActionBindProps = computed(
+    (): Recordable => ({ ...getProps.value, ...advanceState }),
+  );
 </script>
 
 <template>
-  <Form>
-    <Row></Row>
+  <Form
+    v-bind="getBindValue"
+    :class="getFormClass"
+    ref="formElRef"
+    :model="formModel"
+    @keypress.enter="handleEnterPress"
+  >
+    <Row v-bind="getRow">
+      <slot name="formHeader"></slot>
+      <template v-for="schema in getSchema" :key="schema.field">
+        <FormItem
+          :tableAction="tableAction"
+          :formActionType="formActionType"
+          :schema="schema"
+          :formProps="getProps"
+          :allDefaultValues="defaultValueRef"
+          :formModel="formModel"
+          :setFormModel="setFormModel"
+        >
+          <template #[item]="data" v-for="item in Object.keys($slots)">
+            <slot :name="item" v-bind="data || {}"></slot>
+          </template>
+        </FormItem>
+      </template>
+
+      <FormAction v-bind="getFormActionBindProps" @toggle-advanced="handleToggleAdvanced">
+        <template
+          #[item]="data"
+          v-for="item in ['resetBefore', 'submitBefore', 'advanceBefore', 'advanceAfter']"
+        >
+          <slot :name="item" v-bind="data || {}"></slot>
+        </template>
+      </FormAction>
+      <slot name="formFooter"></slot>
+    </Row>
   </Form>
 </template>
 
