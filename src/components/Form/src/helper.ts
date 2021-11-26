e6d0c002d078b8374d26b9b3ab28eb27a12b3284
lhj <1871731528@qq.com>
2021-11-26 10:28:43 +0800

    feat: add date type

---
+++ src/components/Form/src/helper.ts
@@ -0,0 +1,7 @@
+const DATE_TYPE = ['DatePicker', 'MonthPicker', 'WeekPicker', 'TimePicker'];
+
+function genType() {
+  return [...DATE_TYPE, 'RangePicker'];
+}
+
+export const dateItemType = genType();
