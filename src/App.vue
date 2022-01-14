d600a6b85192db6518abb49ba8adb71655d5548b
lhj <1871731528@qq.com>
2022-01-14 20:02:01 +0800

    feat(App-component): Add the ability to switch light/dark modes automatically

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
   import { ConfigProvider } from 'ant-design-vue';
   import { AppProvider } from '/@/components/Application';
+  import { useRootSetting } from './hooks/setting/useRootSetting';
   import { useTitle } from '/@/hooks/web/useTitle';
+  import { updateHeaderBgColor, updateSidebarBgColor } from '/@/logics/theme/updateBackground';
+  import { updateDarkTheme } from '/@/logics/theme/dark';
   import { useLocale } from '/@/locales/useLocale';
   import 'dayjs/locale/zh-cn';
+  import { ThemeEnum } from './enums/appEnum';
 
   const { getAntdLocale } = useLocale();
   useTitle();
+  const { setDarkMode } = useRootSetting();
+  if (window.matchMedia('(prefers-color-scheme: dark)')) {
+    setDarkMode(ThemeEnum.DARK);
+    updateDarkTheme(ThemeEnum.DARK);
+    updateHeaderBgColor();
+    updateSidebarBgColor();
+  } else {
+    setDarkMode(ThemeEnum.LIGHT);
+    updateDarkTheme(ThemeEnum.LIGHT);
+    updateHeaderBgColor();
+    updateSidebarBgColor();
+  }
 </script>
 <template>
   <ConfigProvider :locale="getAntdLocale">
