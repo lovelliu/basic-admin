ea6e31242148876a93704a607e7404918fb18827
lhj <1871731528@qq.com>
2022-02-07 18:48:29 +0800

    refactor(axios):  adjust code structure

--- types/axios.d.ts
+++ types/axios.d.ts
@@ -1,3 +1,30 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export interface CreateAxiosOptions extends AxiosRequestConfig {
+  authenticationScheme?: string;
+  transform?: AxiosTransform;
+  requestOptions?: RequestOptions;
+}
+
+export abstract class AxiosTransform {
+  beforeRequestHook?: (config: AxiosRequestConfig, options: RequestOptions) => AxiosRequestConfig;
+
+  transformRequestHook?: (res: AxiosResponse<Result>, options: RequestOptions) => any;
+
+  requestCatchHook?: (e: Error, options: RequestOptions) => Promise<any>;
+
+  requestInterceptors?: (
+    config: AxiosRequestConfig,
+    options: CreateAxiosOptions,
+  ) => AxiosRequestConfig;
+
+  responseInterceptors?: (res: AxiosResponse<any>) => AxiosResponse<any>;
+
+  requestInterceptorsCatch?: (error: Error) => void;
+
+  responseInterceptorsCatch?: (error: Error) => void;
+}
+
 export type ErrorMessageMode = 'none' | 'modal' | 'message' | undefined;
 
 export interface RequestOptions {
