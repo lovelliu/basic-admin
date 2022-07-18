0005b10ca360b4caea8e4cdf2bd62280b6729ba9
lhj <1871731528@qq.com>
2022-07-18 11:46:41 +0800

    feat: migrate windicss to unocss

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,9 +1,8 @@
 import type { Plugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import WindiCSS from 'vite-plugin-windicss';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import purgeIcons from 'vite-plugin-purge-icons';
 import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import Unocss from 'unocss/vite';
 import { configStyleImportPlugin } from './styleImport';
 import { configHtmlPlugin } from './html';
 import { configSvgIconsPlugin } from './svgSprite';
@@ -12,12 +11,11 @@ import { configThemePlugin } from './theme';
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins: (Plugin | Plugin[])[] = [vue(), vueJsx(), vueSetupExtend()];
 
-  vitePlugins.push(WindiCSS());
+  vitePlugins.push(Unocss());
   vitePlugins.push(configStyleImportPlugin(isBuild));
   vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
   vitePlugins.push(configSvgIconsPlugin(isBuild));
   vitePlugins.push(configThemePlugin(isBuild));
-  vitePlugins.push(purgeIcons());
 
   return vitePlugins;
 }
