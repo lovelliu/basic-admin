2bfec6130b7733099576cbd5011cb3bdd2c4d135
lhj <1871731528@qq.com>
2022-08-01 12:55:04 +0800

    chore: type

--- build/scripts/buildConf.ts
+++ build/scripts/buildConf.ts
@@ -1,9 +1,9 @@
 /**
  * Generate additional configuration files when used for packaging. The file can be configured with some global variables, so that it can be changed directly externally without repackaging
  */
-import { GLOB_CONFIG_FILE_NAME, OUTPUT_DIR } from '../constants';
 import fs, { writeFileSync } from 'fs-extra';
 import chalk from 'chalk';
+import { GLOB_CONFIG_FILE_NAME, OUTPUT_DIR } from '../constants';
 
 import { getEnvConfig, getRootPath } from '../utils';
 import { getConfigFileName } from '../getConfigFileName';
@@ -32,12 +32,13 @@ function createConfig(params: CreateConfigParams) {
     writeFileSync(getRootPath(`${OUTPUT_DIR}/${configFileName}`), configStr);
 
     console.log(
-      chalk.cyan(`✨ [${pkg.name}]`) + ` - configuration file is build successfully:`,
+      `${chalk.cyan(`✨ [${pkg.name}]`)} - configuration file is build successfully:`,
     );
-    console.log(chalk.gray(OUTPUT_DIR + '/' + chalk.green(configFileName)) + '\n');
-  } catch (error) {
+    console.log(`${chalk.gray(`${OUTPUT_DIR}/${chalk.green(configFileName)}`)}\n`);
+  }
+  catch (error) {
     console.log(
-      chalk.red('configuration file configuration file failed to package:\n' + error),
+      chalk.red(`configuration file configuration file failed to package:\n${error}`),
     );
   }
 }
--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import purgeIcons from 'vite-plugin-purge-icons';
 import VueI18n from '@intlify/vite-plugin-vue-i18n';
 import Unocss from 'unocss/vite';
 import { configStyleImportPlugin } from './styleImport';
@@ -15,6 +16,7 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
 
   vitePlugins.push(VueI18n({ runtimeOnly: true, compositionOnly: true, include: [path.resolve(process.cwd(), 'src/locales/lang/**')] }));
   vitePlugins.push(Unocss());
+  vitePlugins.push(purgeIcons());
   vitePlugins.push(configStyleImportPlugin(isBuild));
   vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
   vitePlugins.push(configSvgIconsPlugin(isBuild));
