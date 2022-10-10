063d68ea490f4bd09d05bea657fdd31a75eaa2fc
Lovell Liu <lovelliu@foxmail.com>
2022-10-10 22:43:43 +0800

    fix: resize the svg

--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -27,11 +27,9 @@ setTimeout(() => {
       </div>
       <div class="lg:w-3/10 w-full enter-y">
         <QuickNav :loading="loading" class="enter-y" />
-
         <Card class="!my-4 enter-y" :loading="loading">
-          <img class="xl:h-50 h-30 mx-auto" src="../../../assets/svg/illustration.svg">
+          <img class="xl:h-50 h-30 max-w-full mx-auto" src="../../../assets/svg/illustration.svg">
         </Card>
-
         <SaleRadar :loading="loading" class="enter-y" />
       </div>
     </div>
